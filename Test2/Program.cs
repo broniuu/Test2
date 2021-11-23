32dab6bba0a7cf5f14a21c8db751dbc74fd4d11f
broniuu <86781217+broniuu@users.noreply.github.com>
2021-11-23 15:42:05 +0100

    Feature lines (#1)
    * add First Line
    
    * add second Line
    
    * add second Line
    
    * add Fourth Line
    
    * add Fifth Line
    
    * add Sixth Line

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -7,6 +7,12 @@ namespace Test2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("First Line");
+            Console.WriteLine("Second Line");
+            Console.WriteLine("Third Line");
+            Console.WriteLine("Fourth Line");
+            Console.WriteLine("Fifth Line");
+            Console.WriteLine("Sixth");
         }
     }
 }
